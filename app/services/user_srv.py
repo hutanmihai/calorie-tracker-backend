ea06d64c48e13ec0750c23eda8bf11e69c9f0230
mihaihutan <hutanmihai29@gmail.com>
2023-04-21 18:42:57 +0300

    Add user endpoints, update, delete, list

--- app/services/user_srv.py
+++ app/services/user_srv.py
@@ -1,12 +1,14 @@
+from typing import List
 from uuid import UUID
 
 from fastapi import Depends
 
+from app.apis.utils.enums import HeightMetric, WeightMetric
 from app.models import User
-from app.repositories.errors import EntityNotFound, EntityNotUnique
+from app.repositories.errors import EntityNotFound, EntityNotUnique, EnumValueInvalid
 from app.repositories.user_repo import UserRepository
 from app.services.abstract_srv import AbstractService
-from app.services.errors import UserAlreadyExists, UserNotFound
+from app.services.errors import UserAlreadyExists, UserEnumValueInvalid, UserNotFound
 
 
 class UserSrv(AbstractService):
@@ -32,5 +34,47 @@ class UserSrv(AbstractService):
     async def get_user_by_email(self, email: str) -> User:
         try:
             return await self._repository.get_by_email(email)
+        except EntityNotFound:
+            raise UserNotFound()
+
+    # Update user
+    async def update_user(
+        self,
+        user_id: UUID,
+        name: str | None,
+        pref_height_metric: HeightMetric | None,
+        height: float | None,
+        pref_weight_metric: WeightMetric | None,
+        weight: float | None,
+        target_weight: float | None,
+        target_calories: int | None,
+    ) -> User:
+        try:
+            instance = await self.get_user(user_id)
+        except EntityNotFound:
+            raise UserNotFound()
+
+        instance.name = name or instance.name
+        instance.pref_height_metric = pref_height_metric or instance.pref_height_metric
+        instance.height = height or instance.height
+        instance.pref_weight_metric = pref_weight_metric or instance.pref_weight_metric
+        instance.weight = weight or instance.weight
+        instance.target_weight = target_weight or instance.target_weight
+        instance.target_calories = target_calories or instance.target_calories
+
+        try:
+            return await self._repository.update(instance)
+        except EnumValueInvalid:
+            raise UserEnumValueInvalid()
+
+    # Get all users
+    async def get_all_users(self) -> List[User]:
+        return await self._repository.list(User)
+
+    # Delete user
+    async def delete_user(self, user_id: UUID) -> None:
+        try:
+            instance = await self._repository.get(User, user_id)
+            await self._repository.delete(instance)
         except EntityNotFound:
             raise UserNotFound()
